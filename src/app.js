464fad35c21d516c46cda8333a270f74ea95a0a5
Marvin Andia <marvin.andia@cignium.com>
2016-12-26 18:04:15 -0500

    welcome.html

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Cookbook Aurelia 1.0';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
+      { route: ['', 'home'], name: 'home',      moduleId: 'components/home/home',      nav: true, title: 'Home' },
       { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
       { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
     ]);
