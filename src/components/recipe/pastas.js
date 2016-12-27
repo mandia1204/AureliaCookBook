7bf1c144fa43e45c73a679f0206ada6238d4a524
Marvin Andia <marvin.andia@cignium.com>
2016-12-27 10:47:25 -0500

    Filter pastas

--- src/components/recipe/pastas.js
+++ src/components/recipe/pastas.js
@@ -1,4 +1,5 @@
 import recipeApi from '../../api/recipeApi';
+import {computedFrom} from 'aurelia-framework';
 
 export class Pastas{
   title = "Nothing says Italy like its food, and nothing says Italian food like pasta. Pasta is integrant part of \
@@ -6,8 +7,25 @@ export class Pastas{
                   it can be considered a staple of international cuisine.";
 
   recipes = [];
+  recipeFilter ="";
+  recommendedFilter =false;
+
+  alerta(){
+    alert(this.recommendedFilter);
+  }
+
+  @computedFrom('recipeFilter','recommendedFilter')
+  get filteredRecipes(){
+    const filters = {"name":this.recipeFilter.toLowerCase(),"onlyRecommended":this.recommendedFilter};
+    return this.recipes.filter(r=> this.filterRecipe(r,filters));
+  }
+
+  filterRecipe(recipe,f){
+    return (recipe.name.toLowerCase().indexOf(f.name)!=-1)
+        && (!f.onlyRecommended || recipe.recommended==true);
+  }
 
   activate(){
-    return recipeApi.getAllRecipes().then(response=>this.recipes=response.data);
+    return recipeApi.getAllRecipes().then(response=>this.recipes=response.data.filter(r=>r.category=="Pastas"));
   }
 }
