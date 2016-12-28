8dad36b8e01cf126c13eb337fb6eb74b434ce562
Marvin Andia <marvin.andia@cignium.com>
2016-12-28 17:12:43 -0500

    Basic navigation

--- src/components/manage/manageRecipePage.js
+++ src/components/manage/manageRecipePage.js
@@ -8,10 +8,11 @@ export class ManageRecipePage{
   }
 
   activate(params){
+    console.log(params.category);
     if(params.id){
       this.recipe = {name:"Jose Alanya",chef:"The cheff",category:"Salads",recommended:true};
     }else{
-      this.recipe = {name:"",chef:"",category:"Pastas",url:"",ingredients:[],recommended:false};
+      this.recipe = {name:"",chef:"",category:params.category,url:"",ingredients:[],recommended:false};
     }
     return recipeApi.getAllCategories().then(response=>this.categories=response.data);
   }
