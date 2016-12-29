47138127d3ece6a5e04009fd4858680ab0a09426
Marvin Andia <marvin.andia@cignium.com>
2016-12-29 17:12:46 -0500

    salad page

---
+++ src/components/recipe/salads.js
@@ -0,0 +1,6 @@
+export class Salads{
+
+  body ="Our salad recipes are a delicious and healthy use different ingredients and seasoning to make one of \
+        the most joyous healthy salad recipes. Find the best green salad recipes, plus trusted recipes for more than \
+        3330 other dinner and picnic salads.";
+}
